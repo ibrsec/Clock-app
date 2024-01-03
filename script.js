08be844361e01230e6226dc230e1bbeb06398675
ibr <ibr.seckin@gmail.com>
2024-01-03 19:13:34 +0300

    Finish Commit

--- script.js
+++ script.js
@@ -4,27 +4,32 @@ const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
 const second = document.querySelector(".second");
 
+const hourHand = document.querySelector(".hour-hand");
+const minuteHand = document.querySelector(".min-hand");
+const secondHand = document.querySelector(".sec-hand");
+
 
 
 timer = setInterval(func,1000);
 func();
 
 
-
 function func(){
-    tarih = new Date();
-    let pretty_hour = tarih.getHours();
-    let pretty_min = tarih.getMinutes();
-    let pretty_sec = tarih.getSeconds();
+    date = new Date();
 
-    if(tarih.getHours() < 10){
-        pretty_hour = "0" + String(tarih.getHours());
+    //digital
+    let pretty_hour = date.getHours();
+    let pretty_min = date.getMinutes();
+    let pretty_sec = date.getSeconds();
+
+    if(date.getHours() < 10){
+        pretty_hour = "0" + String(date.getHours());
     }
-    if(tarih.getMinutes() < 10){
-        pretty_min = "0" + String(tarih.getMinutes());
+    if(date.getMinutes() < 10){
+        pretty_min = "0" + String(date.getMinutes());
     }
-    if(tarih.getSeconds() < 10){
-        pretty_sec = "0" + String(tarih.getSeconds());
+    if(date.getSeconds() < 10){
+        pretty_sec = "0" + String(date.getSeconds());
     }
 
 
@@ -32,13 +37,34 @@ function func(){
     hour.innerHTML = pretty_hour;
     minute.innerHTML = pretty_min;
     second.innerHTML = pretty_sec;
-    // console.log(tarih.getFullYear()); // Yılı al
-    // console.log(tarih.getMonth());    // Ayı al (0'dan başlar)
-    // console.log(tarih.getDate());     // Günü al
-    // console.log(typeof tarih.getHours());    // Saati al
-    // console.log(typeof tarih.getMinutes());  // Dakikayı al
-    // console.log(typeof tarih.getSeconds());  // Saniyeyi al
+    
+
+
+
+
+    //Analog 
+
+
+    let second2 = (15 + date.getSeconds()) * 6;
+    let minute2 = (15 + date.getMinutes()) * 6;
+    let hour2 = (15 + date.getHours()) * 30;
+
+
+
+    console.log("derece " + hour2 + ":" + minute2 + ":" + second2);
+    console.log("gercek" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+    console.log("-------");
+
+    secondHand.style.transform = `rotate(${second2}deg)`;
+    minuteHand.style.transform = `rotate(${minute2}deg)`;
+    hourHand.style.transform = `rotate(${hour2}deg)`;
+    
+
 
     
 }
+
+
+
+
 
